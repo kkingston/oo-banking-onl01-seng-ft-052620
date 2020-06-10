5ccbe5549d07f9e05bf063ebca24a77680587aaa
Kayla Kingston <kkingston09@gmail.com>
2020-06-10 19:43:37 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,9 +1,10 @@
 class BankAccount
-  attr_accessor :name 
+  attr_accessor :name :balance :status 
   
     def initialize(name)
       @name = name 
       @balace = 1000
+      @status = open 
     end 
   
   
