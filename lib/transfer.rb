090d8b36d955e6aa8887d5a928d7ee2792d313a2
Kayla Kingston <kkingston09@gmail.com>
2020-06-10 20:44:40 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -28,6 +28,9 @@ class Transfer
   end
   
   def reverse_transfer
-    
+    if self.status == "complete"
+      receiver.balance -= amount 
+      sender.balance += amount
+      self.status = "reversed"
   end 
 end
